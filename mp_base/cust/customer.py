69456f9c2817998da11a36538f898ffa5910cc9d
skyguyver <lynardsalingujay@yahoo.com>
2017-10-17 13:27:26 +0800

    changes

--- mp_base/cust/customer.py
+++ mp_base/cust/customer.py
@@ -13,6 +13,7 @@ class customer(models.Model):
     name = fields.Char(size=60, string="Customer name", index=True)
     mobile_no = fields.Char(size=20, string="Mobile no")
     date_of_birth = fields.Date(string="Date of Birth")
+    acct_ids = fields.One2many(comodel_name="mp.customer_acct", inverse_name="customer_id", string="Accounts")
     active = fields.Boolean(string="Active", default="True")
 
 class customer_address(models.Model):
@@ -23,3 +24,11 @@ class customer_address(models.Model):
     address1 = fields.Char(size=100)
     city = fields.Char(size=40)
     postcode = fields.Char(size=20)
+
+class customer_acct(models.Model):
+    _name = "mp.customer_acct"
+    _description = "Customer Account"
+
+    name = fields.Char(size=60, string="Account name")
+    account_no = fields.Char(size=20, string="Account number", index=True)
+    customer_id = fields.Many2one(comodel_name="mp.customer", string="Customer", ondelete="restrict")
