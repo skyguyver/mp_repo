122310309526bab340c65c231392b2fc62b81430
skyguyver <lynardsalingujay@yahoo.com>
2017-10-17 10:43:32 +0800

    added "active" soft delete

--- mp_base/cust/customer.py
+++ mp_base/cust/customer.py
@@ -1,13 +1,19 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class customer(models.Model):
     _name = 'mp.customer'
     _description = 'Customer'
 
+    # Perform a soft delete
+    @api.multi
+    def unlink(self):
+        for rec in self:
+            rec.active = False
+
     name = fields.Char(size=60, string="Customer name", index=True)
     mobile_no = fields.Char(size=20, string="Mobile no")
     date_of_birth = fields.Date(string="Date of Birth")
-
+    active = fields.Boolean(string="Active", default="True")
 
 class customer_address(models.Model):
     _name = 'mp.customer_address'
